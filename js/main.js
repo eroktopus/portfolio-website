fc41c304ceb60a63107f3f66a144d969050e2a92
eroktopus <147647841+eroktopus@users.noreply.github.com>
2024-06-03 19:48:32 -0400

    fixing mobile compatibility

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   const hamburgerMenu = document.querySelector(".hamburger-menu");
+//   const navigationList = document.querySelector(".navigation-list");
+
+//   hamburgerMenu.addEventListener("click", function () {
+//     navigationList.classList.toggle("open");
+//   });
+// });
